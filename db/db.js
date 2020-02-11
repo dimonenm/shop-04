4ce0916b5267e87dcfc5dc9562c3ab2ea9847497
dimonenm <51917746+dimonenm@users.noreply.github.com>
2020-02-11 13:03:11 +0300

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,31 @@
+var db = {
+    "cement":
+    [
+        {"id":1,"name":"Портландцемент М500, мешок 50 кг, «Masterok»","img":"image/products/cement/1portlandcement-m500-masterok.jpg","imgAlt":"Цемент М500","category":"cement","price":"255р","count":9999},
+        {"id":2,"name":"Цемент М500 Д20 (ЦЕМ II/A-Ш 42,5Н), мешок 25 кг, «BAUFF»","img":"image/products/cement/2m500-d20-bauff-25-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"150р","count":9999},
+        {"id":3,"name":"Цемент М500 Д20 (ЦЕМ II/A-Ш 42,5Н), мешок 40 кг, «BAUFF»","img":"image/products/cement/3m500-d20-bauff-40-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"210р","count":9999},
+        {"id":4,"name":"Цемент М500 Д20 (ЦЕМ II/A-Ш 42,5Н), мешок 50 кг, «BAUFF»","img":"image/products/cement/4m500-d20-bauff-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"250р","count":9999},
+        {"id":5,"name":"Цемент М500 Д0 (ЦЕМ I 42,5Н), мешок 50 кг, «Осколцемент»","img":"image/products/cement/5m500-d20-oskolcement-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"255р","count":9999},
+        {"id":6,"name":"Цемент М500 Д20 (ЦЕМ II/А-Ш 42,5Н), мешок 50 кг, «Себряковцемент»","img":"image/products/cement/6m500-d20-sebryakovcement-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"225р","count":9999},
+        {"id":7,"name":"Цемент М400 Д20 (ЦЕМ II/А-Ш 32,5Б), мешок 50 кг, «Михайловцемент»","img":"image/products/cement/7m400-d20-min.jpg","imgAlt":"Цемент М400","category":"cement","price":"215р","count":9999},
+        {"id":8,"name":"Цемент М500 Д20Б, мешок 50 кг, «Lafarge»","img":"image/products/cement/8m500-d20b-lafarge-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"280р","count":9999},
+        {"id":9,"name":"Цемент М400 Д20 (ЦЕМ II/А-Ш 32,5 Н), мешок 50 кг, «ЕВРОЦЕМЕНТ групп»","img":"image/products/cement/9m400-d20-evrocement-min.jpg","imgAlt":"Цемент М400","category":"cement","price":"255р","count":9999},
+        {"id":10,"name":"Цемент М500 Д20 (ЦЕМ II/А-Ш 42,5Н), мешок 50 кг, «ЕВРОЦЕМЕНТ групп»","img":"image/products/cement/10m500-d20-evrocement-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"265р","count":9999},
+        {"id":11,"name":"Цемент М500 Д0 (ЦЕМ I 42,5H), мешок 50 кг, ТМ «UMIX»","img":"image/products/cement/11m500-d0-umix-min.jpg","imgAlt":"Цемент М500","category":"cement","price":"275р","count":9999}
+    ],
+        "glue":
+    [
+        {"id":1,"name":"Плиточный клей экономкласса KL-105, мешок 25 кг, «Umix»","img":"image/products/glue/1kl-105-min.jpg","imgAlt":"KL-105","category":"glue","price":"148р","count":9999},
+        {"id":2,"name":"Монтажный клей для пеноблоков KL-110 Block, мешок 25 кг, «Umix»","img":"image/products/glue/2kl-110-block-min.jpg","imgAlt":"KL-110","category":"glue","price":"150р","count":9999},
+        {"id":3,"name":"Монтажный клей для пеноблоков KL-110 Block (зима), мешок 25 кг, «Umix»","img":"image/products/glue/3kl-110-block-zim-min.jpg","imgAlt":"KL-110","category":"glue","price":"160р","count":9999},
+        {"id":4,"name":"Клей гипсовый KL-115 Gips, мешок 20 кг, «Umix»","img":"image/products/glue/4kl-115-gips-min.jpg","imgAlt":"KL-115","category":"glue","price":"205p","count":9999},
+        {"id":5,"name":"Клей для блоков КС-06 (Элит), 25 кг, «Финстрой»","img":"image/products/glue/5ks-06-elit.jpg","imgAlt":"КС-06","category":"glue","price":"135p","count":9999},
+        {"id":6,"name":"Клей для плитки КР-07 (Стандарт), 25 кг, «Финстрой»","img":"image/products/glue/6kr-07-finstroy.jpg","imgAlt":"КР-07","category":"glue","price":"150p","count":9999}
+    ],
+        "putty":
+    [
+        {"id":1,"name":"Белая гипсовая штукатурка SHG-80 White, ручного нанесения, мешок 30 кг, «Umix»","img":"image/products/putty/1shg-80-white-min.jpg","imgAlt":"SHG-80 White","category":"putty","price":"280p","count":9999},
+        {"id":2,"name":"Белая гипсовая штукатурка SHG-80 White, машинного нанесения, мешок 30 кг, «Umix»","img":"image/products/putty/2shg-80-lux-min.jpg","imgAlt":"SHG-80 White","category":"putty","price":"285p","count":9999},
+        {"id":3,"name":"Белая гипсовая штукатурка класса люкс SHG-80 Lux, мешок 30 кг, «Umix»","img":"image/products/putty/3shc-90-min.jpg","imgAlt":"SHG-80 White","category":"putty","price":"340p","count":9999}
+    ]
+}
